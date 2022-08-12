ad8ab5ec6f765d7c72a8c6e0c8e3ab8c39eebd5e
aha-oretama <sekine_y_529@msn.com>
2022-08-13 00:43:34 +0900

    9.2. implement reinforce

---
+++ ch09/reinforce.py
@@ -0,0 +1,79 @@
+import gym
+import numpy as np
+from dezero import Model, layers, functions, optimizers
+
+
+class Policy(Model):
+
+    def __init__(self, action_size):
+        super().__init__()
+        self.l1 = layers.Linear(128)
+        self.l2 = layers.Linear(action_size)
+
+    def forward(self, x):
+        y = functions.relu(self.l1(x))
+        y = functions.softmax(self.l2(y))
+        return y
+
+
+class Agent:
+    def __init__(self) -> None:
+        self.gamma = 0.98
+        self.lr = 0.0002
+        self.action_size = 2
+
+        self.memory = []
+        self.pi = Policy(self.action_size)
+        self.optimizer = optimizers.Adam(self.lr)
+        self.optimizer.setup(self.pi)
+
+    def get_action(self, state):
+        state = state[np.newaxis, :]
+        probs = self.pi(state)
+        probs = probs[0]
+        action = np.random.choice(len(probs), p=probs.data)
+        return action, probs[action],
+
+    def add(self, reward, prob):
+        data = (reward, prob)
+        self.memory.append(data)
+
+    def update(self):
+        self.pi.cleargrads()
+
+        G, loss = 0, 0
+        for reward, prob in reversed(self.memory):
+            G = reward + self.gamma * G
+            loss += - G * functions.log(prob)
+
+        loss.backward()
+        self.optimizer.update()
+        self.memory = []
+
+episodes = 3000
+env = gym.make('CartPole-v0')
+agent = Agent()
+reward_history = []
+
+for episode in range(episodes):
+    state = env.reset()
+    done = False
+    total_reward = 0
+
+    while not done:
+        action, prob = agent.get_action(state)
+        next_state, reward, done, info = env.step(action)
+
+        agent.add(reward, prob)
+        state = next_state
+        total_reward += reward
+
+    agent.update()
+    reward_history.append(total_reward)
+    if episode % 100 == 0:
+        print("episode :{}, total reward : {:.1f}".format(episode, total_reward))
+
+
+# plot
+from common.utils import plot_total_reward
+plot_total_reward(reward_history)
